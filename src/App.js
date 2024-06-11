39b26eb9e4781a323bc9b44f28c02d5996b9bd54
Shubham Mondal <shubhamank002@gmail.com>
2024-06-11 18:20:57 +0000

    test json-server

--- src/App.js
+++ src/App.js
@@ -5,34 +5,45 @@ import RevisionPage from "./components/RevisionPage";
 import "./App.css";
 
 const App = () => {
-    const [words, setWords] = useState([]);
+  const [words, setWords] = useState([]);
 
-    useEffect(() => {
-        fetch("/data.json")
-            .then((response) => response.json())
-            .then((data) => setWords(data));
-    }, []);
+  const getTodo = async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/words`);
+      const data = await res.json();
+      setWords(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    <h1 className="logo">
-                        <Link to="/">Vocabrain</Link>
-                    </h1>
-                    <div className="links">
-                        <Link to="/revision">
-                            <button>Revision</button>
-                        </Link>
-                    </div>
-                </header>
-                <Routes>
-                    <Route path="/" element={<MainScreen words={words} />} />
-                    <Route path="/revision" element={<RevisionPage words={words} />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  useEffect(() => {
+    getTodo();
+    // fetch("/data.json")
+    //   .then((response) => response.json())
+    //   .then((data) => setWords(data["words"]));
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="logo">
+            <Link to="/">Vocabrain</Link>
+          </h1>
+          <div className="links">
+            <Link to="/revision">
+              <button>Revision</button>
+            </Link>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<MainScreen words={words} />} />
+          <Route path="/revision" element={<RevisionPage words={words} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
