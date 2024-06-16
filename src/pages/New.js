0beb89df6c815728e15b1d041088073ec3631b7e
Shubham Mondal <shubhamank002@gmail.com>
2024-06-16 17:36:35 +0530

    remove ows form in new page

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,12 +1,10 @@
 import React from "react";
 import NewWord from "../word/NewWord";
-import NewOWS from "../ows/NewOWS";
 
 const New = () => {
     return (
         <div>
             <NewWord />
-            <NewOWS />
         </div>
     );
 };
