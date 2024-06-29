eadd50ca2547ab01d720698638d478a5d32de5c3
Shubham Mondal <shubhamank002@gmail.com>
2024-06-29 20:10:49 +0530

    remove searchbar from dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext, useRef } from "react";
 import { UpdateContext } from "../context/UpdateContext";
-import { FaSearch } from "react-icons/fa";
+import { SearchContext } from "../context/SearchContext";
 import Loader from "../components/Loader";
 import WordContainer from "./word/WordContainer";
 import { getAllWords } from "../utils/utils";
@@ -10,10 +10,10 @@ import "../css/Dashboard.css";
 
 const Dashboard = () => {
     const { wordUpdate } = useContext(UpdateContext);
+    const { searchQuery } = useContext(SearchContext);
 
     const [words, setWords] = useState([]);
     const [allWords, setAllWords] = useState(new Set());
-    const [searchQuery, setSearchQuery] = useState("");
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
@@ -73,49 +73,31 @@ const Dashboard = () => {
 
     return (
         <>
-            <div className="dashboard-container">
-                <div className="main-content">
-                    <div className="main-container-top">
-                        <div className="main-container-sub-top">
-                            <div className="search-bar">
-                                <FaSearch className="search-icon" />
-                                <input
-                                    type="text"
-                                    placeholder="Search..."
-                                    value={searchQuery}
-                                    onChange={(e) => setSearchQuery(e.target.value)}
-                                    className="search-bar"
-                                />
-                            </div>
+            <div className="main-container">
+                <div className="main-container-list">
+                    {filteredWords.map((word) => (
+                        <div
+                            key={word._id}
+                            ref={(el) => {
+                                const firstLetter = word.word.charAt(0).toUpperCase();
+                                if (searchQuery === "" && !wordRefs.current[firstLetter]) {
+                                    wordRefs.current[firstLetter] = el;
+                                } else if (searchQuery !== "") {
+                                    wordRefs.current[firstLetter] = null;
+                                }
+                            }}
+                        >
+                            <WordContainer key={word._id} word={word} allWords={allWords} />
                         </div>
-                    </div>
-                    <div className="main-container">
-                        <div className="main-container-list">
-                            {filteredWords.map((word) => (
-                                <div
-                                    key={word._id}
-                                    ref={(el) => {
-                                        const firstLetter = word.word.charAt(0).toUpperCase();
-                                        if (searchQuery === "" && !wordRefs.current[firstLetter]) {
-                                            wordRefs.current[firstLetter] = el;
-                                        } else if (searchQuery !== "") {
-                                            wordRefs.current[firstLetter] = null;
-                                        }
-                                    }}
-                                >
-                                    <WordContainer key={word._id} word={word} allWords={allWords} />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className="alphabet-selector">
-                    {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => (
-                        <button key={letter} onClick={() => handleAlphabetClick(letter)}>
-                            {letter}
-                        </button>
                     ))}
                 </div>
+            </div>
+            <div className="alphabet-selector">
+                {"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").map((letter) => (
+                    <button key={letter} onClick={() => handleAlphabetClick(letter)}>
+                        {letter}
+                    </button>
+                ))}
             </div>
         </>
     );
