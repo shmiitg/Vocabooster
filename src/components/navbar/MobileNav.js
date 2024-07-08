55015d16e0ad6c7aa77cb7902b4d0754e84cc4c0
Shubham Mondal <shubhamank002@gmail.com>
2024-07-08 12:38:17 +0530

    refactor mobile-nav code

--- src/components/navbar/MobileNav.js
+++ src/components/navbar/MobileNav.js
@@ -1,106 +1,41 @@
 import React, { useState, useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
-import {
-    FaHome,
-    FaBook,
-    FaBrain,
-    FaSignOutAlt,
-    FaSignInAlt,
-    FaUser,
-    FaChevronDown,
-    FaChevronUp,
-} from "react-icons/fa";
+import MobileNavItem from "./MobileNavItem";
+import { navMenu } from "../../utils/utils";
+import { FaSignOutAlt } from "react-icons/fa";
 import { AuthContext } from "../../context/AuthContext";
 
 const MobileNav = ({ isOpen, setOpen }) => {
-    const navLinks = [
-        { name: "Home", address: "/", icon: <FaHome /> },
-        { name: "Revise", address: "/revision", icon: <FaBook /> },
-        { name: "OWS", address: "/ows", icon: <FaBrain /> },
-        { name: "Idiom", address: "/idioms", icon: <FaBook /> },
-        { name: "Spelling", address: "/spellings", icon: <FaBook /> },
-        { name: "Cluster", address: "/clusters", icon: <FaBook /> },
-    ];
-    const userLinks = [
-        { name: "Word", address: "/bookmark" },
-        { name: "OWS", address: "/bookmark-ows" },
-    ];
-    const location = useLocation();
-    const pathname = location.pathname;
     const { user, logout } = useContext(AuthContext);
-    const [isSubmenuOpen, setSubmenuOpen] = useState(false);
 
     const handleMenuToggle = () => {
         setOpen((prev) => !prev);
-    };
-
-    const handleSubmenuToggle = () => {
-        setSubmenuOpen((prev) => !prev);
     };
 
     return (
         <>
             <div className={`hamburger-menu ${isOpen ? "open" : ""}`}>
                 <div className="hamburger-links">
-                    {navLinks.map((navlink, index) => (
-                        <div
-                            key={index}
-                            className={
-                                pathname === navlink.address
-                                    ? "hamburger-link-active"
-                                    : "hamburger-link"
-                            }
-                            onClick={handleMenuToggle}
-                        >
-                            <Link to={navlink.address}>
-                                {navlink.icon}
-                                {navlink.name}
-                            </Link>
-                        </div>
-                    ))}
-                    {user ? (
-                        <>
-                            <div className="hamburger-link" onClick={handleSubmenuToggle}>
-                                <span>
-                                    <FaUser />
-                                    Bookmark
-                                    {isSubmenuOpen ? <FaChevronUp /> : <FaChevronDown />}
-                                </span>
-                            </div>
-                            {isSubmenuOpen && (
-                                <div className="submenu">
-                                    {userLinks.map((userlink, index) => (
-                                        <div
-                                            key={index}
-                                            className={
-                                                pathname === userlink.address
-                                                    ? "hamburger-link-active"
-                                                    : "hamburger-link"
-                                            }
-                                        >
-                                            <Link to={userlink.address}>{userlink.name}</Link>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                            <div className="hamburger-link" onClick={logout}>
-                                <span>
-                                    <FaSignOutAlt />
-                                    Logout
-                                </span>
-                            </div>
-                        </>
-                    ) : (
-                        <div
-                            className={
-                                pathname === "/login" ? "hamburger-link-active" : "hamburger-link"
-                            }
-                            onClick={handleMenuToggle}
-                        >
-                            <Link to="/login">
-                                <FaSignInAlt />
-                                Login
-                            </Link>
+                    {navMenu.map((navItem, index) => {
+                        if (navItem.requiresAuth && !user) {
+                            return null;
+                        }
+                        if (navItem.guestOnly && user) {
+                            return null;
+                        }
+                        return (
+                            <MobileNavItem
+                                key={index}
+                                navItem={navItem}
+                                handleMenuToggle={handleMenuToggle}
+                            />
+                        );
+                    })}
+                    {user && (
+                        <div className="hamburger-link" onClick={logout}>
+                            <span>
+                                <FaSignOutAlt />
+                                Logout
+                            </span>
                         </div>
                     )}
                 </div>
---
+++ src/components/navbar/MobileNavItem.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+
+const MobileNavItem = ({ navItem, handleMenuToggle }) => {
+    const location = useLocation();
+    const pathname = location.pathname;
+
+    const [dropdown, setDropdown] = useState(false);
+
+    const toggleDropdown = () => setDropdown((prev) => !prev);
+
+    return (
+        <div>
+            {navItem.submenu ? (
+                <>
+                    <div className="hamburger-link" onClick={toggleDropdown}>
+                        <span>
+                            {navItem.icon}
+                            {navItem.title}
+                            {dropdown ? <FaChevronUp /> : <FaChevronDown />}
+                        </span>
+                    </div>
+                    {dropdown && (
+                        <div className="submenu">
+                            {navItem.submenu.map((item, index) => (
+                                <div
+                                    key={index}
+                                    className={
+                                        pathname === item.address
+                                            ? "hamburger-link-active"
+                                            : "hamburger-link"
+                                    }
+                                    onClick={handleMenuToggle}
+                                >
+                                    <Link to={item.address}>{item.title}</Link>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </>
+            ) : (
+                <div
+                    className={
+                        pathname === navItem.address ? "hamburger-link-active" : "hamburger-link"
+                    }
+                    onClick={handleMenuToggle}
+                >
+                    <Link to={navItem.address}>
+                        {navItem.icon}
+                        {navItem.title}
+                    </Link>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default MobileNavItem;
