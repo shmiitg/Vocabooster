002e31e96520b730e4263b3371600b1735ed1771
Shubham Mondal <shubhamank002@gmail.com>
2024-12-20 20:24:45 +0530

    revert index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -6,18 +6,9 @@ import { SearchProvider } from "./context/SearchContext";
 import { AuthProvider } from "./context/AuthContext";
 import UpdateProvider from "./context/UpdateContext";
 
-const Root = () => {
-    useEffect(() => {
-        // Add Google AdSense script dynamically to the head of the document
-        const script = document.createElement("script");
-        script.src =
-            "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1588856948014272";
-        script.async = true;
-        script.crossOrigin = "anonymous";
-        document.head.appendChild(script);
-    }, []);
-
-    return (
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
         <Router>
             <SearchProvider>
                 <UpdateProvider>
@@ -27,12 +18,5 @@ const Root = () => {
                 </UpdateProvider>
             </SearchProvider>
         </Router>
-    );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <Root />
     </React.StrictMode>
 );
