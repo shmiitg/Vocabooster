b0a540a1e02a940f413bfbce6cc8d6e49bc67910
Shubham Mondal <shubhamank002@gmail.com>
2024-12-20 20:13:41 +0530

    add googleadsense

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -7,6 +7,17 @@ import { AuthProvider } from "./context/AuthContext";
 import UpdateProvider from "./context/UpdateContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+useEffect(() => {
+    // Add AdSense script dynamically to the head of the document
+    const script = document.createElement("script");
+    script.src =
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1588856948014272";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+}, []);
+
 root.render(
     <React.StrictMode>
         <Router>
